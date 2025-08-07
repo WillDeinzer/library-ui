79c4e54e99404b1470816565e2ba1243426f3830
Will Deinzer <willdeinzer@gmail.com>
2025-08-06 23:30:42 -0400

    Book cards

--- src/app/bookCatalog/page.tsx
+++ src/app/bookCatalog/page.tsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from "react";
 import { Book } from "../types/types";
 import { getData } from "../services/apiService";
+import BookCard from "../components/book/bookCard";
+import { Box, Button, Card, Container, Flex, Group, Paper, SimpleGrid, Title } from "@mantine/core";
 
 export default function BookCatalogPage() {
     const [books, setBooks] = useState<Book[]>([]);
@@ -23,6 +25,30 @@ export default function BookCatalogPage() {
     }, []);
 
     return (
-        <div>Book Catalog Page</div>
-    )
+        <Container size="xl" py="md">
+            <Paper
+                shadow="xs"
+                p="sm"
+                style={{ width: '100%', marginTop: '0', marginBottom: '1rem' }}
+            >
+                <Flex justify="space-between" align="center">
+                <Box style={{ width: '100px' }} /> {/* Invisible spacer */}
+                <Title order={3} style={{ textAlign: 'center', flexGrow: 1 }}>
+                    Book Catalog
+                </Title>
+                <Button onClick={() => {/* handle add action */}}>
+                    Add Book
+                </Button>
+                </Flex>
+            </Paper>
+            <SimpleGrid
+                cols={{ base: 1, sm: 2, md: 3, lg: 4}}
+                spacing="md"
+            >
+                {books.map((book) => (
+                <BookCard key={book.isbn} book={book} />
+                ))}
+            </SimpleGrid>
+        </Container>
+    );
 }
--- src/app/components/book/bookCard.tsx
+++ src/app/components/book/bookCard.tsx
@@ -1,13 +1,75 @@
+import { Card, Image, Stack, Title, Text, Button, ActionIcon, Tooltip } from "@mantine/core";
+import { IconArrowRight, IconBookmark, IconBookmarkFilled } from '@tabler/icons-react'
 import { Book } from "../../types/types";
+import { useState } from "react";
 
 interface BookCardProps {
     book: Book
 }
 
 export default function BookCard({ book }: BookCardProps) {
+    const [inWishlist, setInWishlist] = useState(false);
+
+    const handleAddToWishlist = () => {
+        setInWishlist(!inWishlist);
+    }
+
+    const handleSeeMore = () => {
+
+    }
+
     return (
-        <div>
-            Insert Text here
-        </div>
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Tooltip label={inWishlist ? "Remove from wishlist" : "Add to wishlist"}>
+                <ActionIcon
+                    variant="filled"
+                    color={inWishlist ? "red" : 'gray'}
+                    size="lg"
+                    onClick={handleAddToWishlist}
+                    style={{
+                        position: 'absolute',
+                        top: 10,
+                        left: 10,
+                        zIndex: 10
+                    }}
+                >
+                    {inWishlist ? <IconBookmarkFilled size={20} /> : <IconBookmark size={20} />}
+                </ActionIcon>
+            </Tooltip>
+
+            <Tooltip label="See more">
+                <ActionIcon
+                    variant="filled"
+                    color="blue"
+                    size="lg"
+                    onClick={handleSeeMore}
+                    style={{
+                        position: 'absolute',
+                        top: 10,
+                        right: 10,
+                        zIndex: 10
+                    }}
+                >
+                    <IconArrowRight size={20} />
+                </ActionIcon>
+            </Tooltip>
+                
+            
+            <Card.Section>
+                <Image
+                    src={book.image}
+                    alt={"Cover of " + book.title}
+                    height={200}
+                    radius="lg"
+                    fit="contain"
+                    style={{ display: "block", marginTop: "1.5rem", marginBottom: "1rem" }}
+                />
+            </Card.Section>
+
+            <Stack align="center" gap="md" style={{"width": "100%"}}>
+                <Title order={4} style={{ textAlign: "center" }} >{book.title}</Title>
+                <Text size="md">{"By " + book.authors?.[0] }</Text>
+            </Stack>
+        </Card>
     )
 }
